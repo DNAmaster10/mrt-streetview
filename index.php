0387f788e723d516cc1b3ba6222fca45617439d5
DNAmaster10 <louishollobon@outlook.com>
2022-04-17 21:31:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,14 @@
     <style>
         .panorama{
             width:100%;
-            height: 100%;
+            height: 50px;
         }
     </style>
     <body>
         <div id="panorama" class="panorama"></div>
+        <form action="/pages/login/login.php">
+            <input type="submit" value="login">
+        </form>
     </body>
     <script>
     pannellum.viewer("panorama", {

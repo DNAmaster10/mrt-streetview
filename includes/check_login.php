2758e037192b3059e7d78a0fed637d72db4fee7d
DNAmaster10 <louishollobon@outlook.com>
2022-04-18 00:50:46 +0100

    Update check_login.php

--- includes/check_login.php
+++ includes/check_login.php
@@ -1,17 +1,24 @@
 <?php
-    if (!isset($_SESSION["username"] || !isset($_SESSION["password"])) {
+    if (!isset($_SESSION["username"]) || !isset($_SESSION["password"])) {
         header ("location: /pages/session_expired.php");
         die();
     }
     $stmt = $conn->prepare("SELECT password FROM users WHERE username=?");
     $stmt->bind_param("s",$_SESSION["username"]);
-    $raw_result = $stmt->execute();
-    if ($raw_result->num_rows > 0) {
-        $row = $raw_result->fetch_assoc();
-        $result = $row["password"];
-        unset($raw_result);
-        unset($row);
-        $logged_in = true;
+    $stmt->execute();
+    $stmt->bind_result($result);
+    $stnt->fetch();
+    if ($result) {
+        if ($result == $_SESSION["password"]) {
+            $logged_in = true;
+        }
+        else {
+            unset($_SESSION["username"]);
+            unset($_SESSION["password"]);
+            header ("location: /pages/invalid_login.php");
+            $logged_in = false;
+            die();
+        }
     }
     else {
         unset($_SESSION["username"]);
